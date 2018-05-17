90e410fb735e8e540d3f57dc43a370b6679df9ba
Paulo Mariano <pazevedo@stone.com.br>
2018-05-17 20:40:14 -0300

    fix python lint

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -4,6 +4,7 @@ def test_logstash_installed_plugins(Ansible, Command):
     defaults = Ansible("include_vars", "./defaults/main.yml")["ansible_facts"]
     plugins = defaults['logstash_install_plugins']
 
-    logstash_plugins = Command("/usr/share/logstash/bin/logstash-plugin list").stdout
+    logstash_plugins = Command(
+                "/usr/share/logstash/bin/logstash-plugin list").stdout
 
     assert plugins[0] in logstash_plugins
